006c9e9f4bd363bb8c6de29e854e5a9a52a55a10
BenMartFilio <ecobmartinezfilio@gmail.com>
2026-02-15 13:29:04 +0100

    aleatoire skin

--- Assets/Scripts/Objects/ObjectsWhichFall.cs
+++ Assets/Scripts/Objects/ObjectsWhichFall.cs
@@ -1,11 +1,26 @@
+using System.Linq;
 using UnityEngine;
 
 public class ObjectsWhichFall : MonoBehaviour
 {
+    private SpriteRenderer spriteToChange;
+    [SerializeField] private Sprite[] sprites;
+    private int randomNumber;
+    private void Start()
+    {
+        ChangeSkin();
+    }
     public void WhenGetted()
     {
         Destroy(gameObject);
         // faire spawn un game object avec un sprite de récupérer
         // faire spawn son récupéré.
+    }
+
+    public void ChangeSkin()
+    {
+        SpriteRenderer spriteToChange = GetComponent<SpriteRenderer>();
+        randomNumber = Random.Range(0, sprites.Length);
+        spriteToChange.sprite = sprites[randomNumber];
     }
 }
