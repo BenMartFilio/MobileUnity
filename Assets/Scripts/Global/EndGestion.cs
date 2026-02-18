5998f8a9b1c34853b971a6213d15f5b127f05b4e
BenMartFilio <ecobmartinezfilio@gmail.com>
2026-02-18 22:16:29 +0100

    end

--- Assets/Scripts/Global/EndGestion.cs
+++ Assets/Scripts/Global/EndGestion.cs
@@ -1,5 +1,7 @@
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class EndGestion : MonoBehaviour
 {
@@ -7,6 +9,9 @@ public class EndGestion : MonoBehaviour
     [SerializeField] private SO_PlayerDatas playerDatas;
     [SerializeField] private SaveGameSystem save;
     [SerializeField] private TimeManager time;
+    [SerializeField] private TMP_Text scoresToChange;
+    [SerializeField] private GameObject endCanvas;
+    private CanvasGroup canvaGroup = null;
 
     public void OnEndGame()
     {
@@ -17,6 +22,38 @@ public class EndGestion : MonoBehaviour
         }
         save.SaveGame();
         time.StopTime();
+        scoresToChange.text = $"Score : {score} \n High score : {playerDatas.Score1}";
+        ScoringScreenApparition();
         Debug.Log($"Vous avez fait ce score : {score}");
+    }
+
+    public void ScoringScreenApparition()
+    {
+        canvaGroup = endCanvas.GetComponent<CanvasGroup>();
+        StartCoroutine(UnhideEnding());
+    }
+
+    public IEnumerator UnhideEnding()
+    {
+        var aValue = 0f;
+        canvaGroup.blocksRaycasts = true;
+        while (aValue < 1f)
+        {
+            if (canvaGroup != null)
+            {
+                canvaGroup.alpha = Mathf.Lerp(aValue, 1f, 0.6f);
+                aValue += Time.deltaTime;
+            }
+            else
+            {
+                break;
+            }
+
+            yield return null;
+        }
+        canvaGroup.alpha = 1.0f;
+        
+        yield return null;
+        
     }
 }
