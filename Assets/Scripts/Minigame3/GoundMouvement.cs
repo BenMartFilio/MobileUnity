e3cc9c624ebd7839b6a12545bde540374e9b1d92
BenMartFilio <ecobmartinezfilio@gmail.com>
2026-03-15 15:38:31 +0100

    Corrected

--- Assets/Scripts/Minigame3/GoundMouvement.cs
+++ Assets/Scripts/Minigame3/GoundMouvement.cs
@@ -5,6 +5,24 @@ public class GoundMouvement : MonoBehaviour
     public float speed = 5f;
     public float width;
     public bool started = false;
+    [SerializeField] private int maxSpeed = 30;
+
+    [SerializeField] private TimeManager _timeManager;
+
+    private void OnEnable()
+    {
+        _timeManager.OnTimePassed += Acceleration;
+    }
+
+    private void OnDisable()
+    {
+        _timeManager.OnTimePassed -= Acceleration;
+    }
+
+    public void Acceleration()
+    {
+        speed = Mathf.Clamp(speed+1,1,maxSpeed);
+    }
 
     void Update()
     {
